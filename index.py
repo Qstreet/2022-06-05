eaeef49e47cd7c98fbbcbf217ab4d7eeba398c7d
Qstreet <git@qstreet.org>
2022-06-09 16:00:22 +0000

    add nba csv file and index.py

---
+++ index.py
@@ -0,0 +1,17 @@
+# %%
+from urllib import response
+import pandas as pd
+import requests
+
+download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv"
+
+response = requests.get(download_url)
+
+response.status_code
+
+with open('nba_all_elo.csv', 'wb') as f:
+    f.write(response.content)
+
+
+
+# %%
